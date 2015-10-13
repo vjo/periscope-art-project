19ad77cbc2e8626e7d8e9cf040ece62477258b61
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2015-10-13 01:58:02 +0100

    Add input field and connect button to allow live edit
    Fixes #1

--- rainbow/index.jsx
+++ rainbow/index.jsx
@@ -1,16 +1,15 @@
-var peristream = require('peristream/browser');
-var participantColors = require('periscope-participant-colors');
+const peristream = require('peristream/browser');
+const participantColors = require('periscope-participant-colors');
 
-var stream = peristream(''); // Set a Periscope URL
-
-var Message = React.createClass({
+const Message = React.createClass({
   render() {
     return <div className='message' style={{backgroundColor: this.props.color.hex}}></div>
   }
 });
-var MessageList = React.createClass({
+
+const MessageList = React.createClass({
   render() {
-    var messages = this.props.messageList.map(function(message) {
+    const messages = this.props.messageList.map(function(message) {
       return <Message color={message.color} />;
     });
     return (
@@ -20,27 +19,92 @@ var MessageList = React.createClass({
     );
   }
 });
-var MessagesContainer = React.createClass({
+
+const MessagesContainer = React.createClass({
   getInitialState() {
-    return {messageList: []};
+    return {
+      messageList: [],
+      connected: false,
+      connecting: false
+    };
   },
   componentWillMount() {
-    var self = this;
-    stream.connect().then(function(emitter){
-      emitter.on(peristream.HEARTS, function(message){
-        self.addMsg(message);
-      });
+    this.setUrl = this.setUrl.bind(this);
+    this.switchToCurrentUrl = this.switchToCurrentUrl.bind(this);
+  },
+  setUrl(event) {
+    var url = event.target.value;
+    this.setState({
+      url
     });
   },
+  switchToCurrentUrl() {
+    this.disconnect();
+    this.connect();
+  },
+  disconnect() {
+    const { stream } = this.state;
+
+    this.setState({
+      connected: false,
+      connecting: false
+    });
+
+    if (stream) {
+      stream.disconnect();
+    }
+
+  },
+  connect() {
+
+    const { url } = this.state;
+    const stream = peristream(url);
+
+    this.setState({
+      stream,
+      messageList: [],
+      connecting: true,
+      connected: false
+    });
+
+    stream.connect().then(function(emitter){
+      this.setState({
+        connecting: false,
+        connected: true
+      });
+      emitter.on(peristream.HEARTS, function(message){
+        this.addMsg(message);
+      }.bind(this));
+    }.bind(this));
+  },
+
   addMsg(message) {
     message.color = participantColors(message.participant_index);
-    var messageList = this.state.messageList;
-    this.setState({messageList: messageList.concat(message)});
+
+    const { messageList } = this.state;
+    const newMessageList = [...messageList, message];
+
+    this.setState({
+      messageList: newMessageList
+    });
+
   },
   render() {
+
+    const { connecting, connected } = this.state;
+
+    let link;
+
+    if (!connected && !connecting) {
+      link = <a href="https://twitter.com/hashtag/Periscope?src=hash">Find periscope.tv a url here</a>;
+    }
+
     return (
       <div>
         <h1>RainbowScope</h1>
+        <input onChange={this.setUrl}></input>
+        <button onClick={this.switchToCurrentUrl} disabled={connecting}>Make it shine</button>
+        { link }
         <MessageList messageList={this.state.messageList}/>
       </div>
     );
